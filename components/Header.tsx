87498ee33c2077d0c155cfd911cfd114be419334
fhub-1 <flutterhub1.21.0@gmail.com>
2023-09-14 22:50:33 +0300

    working funny

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,11 @@
 import Link from "next/link";
 import Container from "./ui/container";
 import { Button } from "./ui/button";
-import { ShoppingCart, Sun } from "lucide-react";
+import { Menu, ShoppingCart, Sun } from "lucide-react";
 import { FaceIcon, ImageIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import ProfileButton from "./ui/profilebutton";
 import { useTheme } from "next-themes";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 // nav links
 
 const routes = [
@@ -19,7 +20,7 @@ const routes = [
     label: "Categories",
   },
   {
-    href: "/",
+  href: "/sale",
     label: "On sale",
   },
 ];
@@ -31,6 +32,24 @@ export default function Header() {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between w-full">
           <div className="flex items-center">
+            <Sheet>
+              <SheetTrigger>
+                <Menu className="h-6 md:hidden w-6" />
+              </SheetTrigger>
+              <SheetContent side="left" className="w-[300px] sm:w-[400px]">
+                <nav className="flex flex-col gap-4">
+                  {routes.map((route, i) => (
+                    <Link
+                      key={i}
+                      href={route.href}
+                      className="block px-2 py-1 text-lg"
+                    >
+                      {route.label}
+                    </Link>
+                  ))}
+                </nav>
+              </SheetContent>
+            </Sheet>
             <Link href="/" className="ml-4 lg:ml-0">
               <h1 className="text-xl font-bold">NAIVAS</h1>
             </Link>
---
+++ types.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  name: string;
+  price: string;
+  // images: string[];
+  image: string;
+};
